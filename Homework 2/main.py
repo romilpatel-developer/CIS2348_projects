24160a69777dc576ab102c1c1a9d2e2f8dada39a
romilpatel-developer <77759178+romilpatel-developer@users.noreply.github.com>
2021-03-03 00:29:08 -0600

    section 6.17

--- Homework 2/main.py
+++ Homework 2/main.py
@@ -1,108 +1,28 @@
+#Name-Romilkumar Patel
+# PSID-1765483
+# Section 6.17
 
-#Part A
+password = input()
 
-var = 1
-months = ['January','February','March','April','May','June',
-          'July','August','September','October','November','December']
-while var == 1:
-  try:
-    stri = input("Enter the month day, year: ")
-  except SyntaxError:
-    continue
-  if stri=='-1':
-    break
+modified_password = ''
 
-  try:
-    arr = stri.split()
-    digit = len(arr)
-    if digit!=3:
-      continue
-    m = arr[0]
-    day = arr[1]
-    y = arr[2]
-    d, comma = day.split(',')
+i = 0
+while i < len(password):
+    ch = password[i]
+    if ch == 'i':
+        modified_password += '!'
+    elif ch == 'a':
+        modified_password += '@'
+    elif ch == 'm':
+        modified_password += 'M'
+    elif ch == 'B':
+        modified_password += '8'
+    elif ch == 'o':
+        modified_password += '.'
+    else:
+        modified_password += ch
+    i += 1
 
-    for x in range(12):
-      if stri.find(months[x])>=0:
-        mo = str(x+1)
-        ans = mo + '/' + d + '/' + y
-        print(ans)
-        break
-  except ValueError:
-    continue
+modified_password += "q*s"
 
-#Part B
-
-file1 = open('inputDates.txt', 'r')
-dates = file1.readlines()
-
-months = ['January', 'February', 'March', 'April', 'May', 'June',
-          'July', 'August', 'September', 'October', 'November', 'December']
-
-for stri in dates:
-    if stri == '-1':
-        break
-
-    try:
-        arr = stri.split()
-        digit = len(arr)
-        if digit != 3:
-            continue
-        m = arr[0]
-        day = arr[1]
-        y = arr[2]
-        d, comma = day.split(',')
-
-        for x in range(12):
-            if stri.find(months[x]) >= 0:
-                mo = str(x + 1)
-                ans = mo + '/' + d + '/' + y
-                print(ans)
-                break
-    except ValueError:
-        continue
-
-file1.close()
-
-#Part C
-
-file1 = open('inputDates.txt','r')
-dates = file1.readlines()
-
-months = ['January','February','March','April','May','June',
-          'July','August','September','October','November','December']
-
-count = 0
-for stri in dates:
-  count++
-  if stri=='-1':
-    break
-
-  try:
-    arr = stri.split()
-    digit = len(arr)
-    if digit!=3:
-      continue
-    m = arr[0]
-    day = arr[1]
-    y = arr[2]
-    d, comma = day.split(',')
-
-    for x in range(12):
-      if stri.find(months[x])>=0:
-        mo = str(x+1)
-        ans = mo + '/' + d + '/' + y
-        if count>0:
-          file2 = open('parsedDates.txt','a')
-          file2.write("ans \n")
-          file2.close()
-        else:
-          file3 = open('parsedDates.txt','w')
-          file3.write("ans \n")
-          file3.close()
-        break
-
-  except ValueError:
-    continue
-
-file1.close()
+print(modified_password)
